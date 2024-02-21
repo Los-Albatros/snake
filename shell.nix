76b354e4eb43c8e2e6f1a77ef131a858a8604f5d
Steve Roques <steve.roques@gmail.com>
2024-02-21 05:52:34 +0100

    New impl in pygame

--- shell.nix
+++ shell.nix
@@ -1,20 +1,10 @@
-{pkgs ? import <nixpkgs> {}}:
+{ pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-    buildInputs = with pkgs; [ 
-        cargo
-        rustc
-        pkg-config
-        openssl
-        xorg.libX11
-	xorg.libX11.dev
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-        libGL
-    ];
+  buildInputs = with pkgs; [
+    python311
+    python311Packages.pygame
+    python311Packages.appdirs
+  ];
+}
 
-    shellHook = ''
-      export LD_LIBRARY_PATH="${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXi}/lib:${pkgs.libGL}/lib"
-    '';
-  }
